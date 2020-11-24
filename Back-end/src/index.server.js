b9b2eb3dc9b83b2434bc4c0919b5f830b20e42f4
Jay Patel <pateljaypj1397@gmail.com>
2020-11-24 00:36:28 -0500

    User Signup

--- Back-end/src/index.server.js
+++ Back-end/src/index.server.js
@@ -4,35 +4,31 @@ const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
 
+//routes
+const userRoutes = require("./routes/user");
+
 //environment variable or you can say constants
 env.config();
 
 // mongoDB connection
 //mongodb+srv://<username>:<password>@cluster0.bnlm5.mongodb.net/<dbname>?retryWrites=true&w=majority
 mongoose
-  .connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.bnlm5.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.bnlm5.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    }
+  )
   .then(() => {
     console.log("Databse connected");
   });
 
 app.use(bodyParser.urlencoded());
-
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Hello from server",
-  });
-});
-
-app.post("/data", (req, res, next) => {
-  res.status(200).json({
-    message: req.body,
-  });
-});
+app.use("/api", userRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
