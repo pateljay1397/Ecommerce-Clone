d70f623cdc1c50931d32372c2f7bad99cb802ec6
Jay Patel <pateljaypj1397@gmail.com>
2020-11-26 16:58:01 -0500

    Signin and Signup validation

--- Back-end/src/routes/admin/auth.js
+++ Back-end/src/routes/admin/auth.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const { signup, signin, requireSignin } = require("../../controller/admin/auth");
+const {
+  signup,
+  signin,
+  requireSignin,
+} = require("../../controller/admin/auth");
+const {
+  validateSignupRequest,
+  isRequestValidated,
+  validateSigninRequest,
+} = require("../../validators/auth");
 const router = express.Router();
 
-
-router.post("/admin/signup", signup);
-router.post("/admin/signin", signin);
+router.post("/admin/signup", validateSignupRequest, isRequestValidated, signup);
+router.post("/admin/signin", validateSigninRequest, isRequestValidated, signin);
 // router.post("/profile", requireSignin, (req, res) => {
 //   res.status(200).json({ user: "profile" });
 // });
--- Back-end/src/routes/auth.js
+++ Back-end/src/routes/auth.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const { signup, signin, requireSignin } = require("../controller/auth");
 const router = express.Router();
+const { check } = require("express-validator");
+const {
+  validateSignupRequest,
+  isRequestValidated,
+  validateSigninRequest,
+} = require("../validators/auth");
 
-
-router.post("/signup", signup);
-router.post("/signin", signin);
+router.post("/signup", validateSignupRequest, isRequestValidated, signup);
+router.post("/signin", validateSigninRequest, isRequestValidated, signin);
 // router.post("/profile", requireSignin, (req, res) => {
 //   res.status(200).json({ user: "profile" });
 // });
