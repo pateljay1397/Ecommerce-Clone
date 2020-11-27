bac12b157bfc4f58367ac6c8aff8b56506fb8326
Jay Patel <pateljaypj1397@gmail.com>
2020-11-27 17:23:24 -0500

    User can add product with pictures

--- Back-end/src/index.server.js
+++ Back-end/src/index.server.js
@@ -8,6 +8,7 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const adminRoutes = require("./routes/admin/auth");
 const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
 
 //environment variable or you can say constants
 env.config();
@@ -35,6 +36,7 @@ app.use(express.json());
 app.use("/api", authRoutes);
 app.use("/api", adminRoutes);
 app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
