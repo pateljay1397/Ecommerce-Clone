2f95145b6978529f03e95bf69a301117c0879ec0
Jay Patel <pateljaypj1397@gmail.com>
2020-11-27 16:05:14 -0500

    User can create category and fetch from datbase in nested form

--- Back-end/src/controller/category.js
+++ Back-end/src/controller/category.js
@@ -1,5 +1,25 @@
 const Category = require("../models/category");
 const slugify = require("slugify");
+const { create } = require("../models/category");
+
+function createCategories(categories, parentId = null) {
+  const categoryList = [];
+  let category;
+  if (parentId == null) {
+    category = categories.filter((cat) => cat.parentId == undefined);
+  } else {
+    category = categories.filter((cat) => cat.parentId == parentId);
+  }
+  for (let cate of category) {
+    categoryList.push({
+      _id: cate._id,
+      name: cate.name,
+      slug: cate.slug,
+      children: createCategories(categories, cate._id),
+    });
+  }
+  return categoryList;
+}
 
 exports.addCategory = (req, res) => {
   const categoryObj = {
@@ -7,6 +27,7 @@ exports.addCategory = (req, res) => {
     slug: slugify(req.body.name),
   };
 
+  console.log(categoryObj);
   if (req.body.parentId) {
     categoryObj.parentId = req.body.parentId;
   }
@@ -24,7 +45,8 @@ exports.getCategories = (req, res) => {
   Category.find({}).exec((Error, categories) => {
     if (Error) return res.status(400).json({ Error });
     if (categories) {
-      res.status(200).json({ categories });
+      const categoryList = createCategories(categories);
+      res.status(200).json({ categoryList });
     }
   });
 };
