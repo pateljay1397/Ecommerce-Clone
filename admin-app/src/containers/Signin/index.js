5b60f69d9b2b74ad0c92447567eb3ab2c7435877
Jay Patel <pateljaypj1397@gmail.com>
2020-11-29 16:09:08 -0500

    Redux-initial setup

--- admin-app/src/containers/Signin/index.js
+++ admin-app/src/containers/Signin/index.js
@@ -2,14 +2,26 @@ import React from "react";
 import { Button, Row, Col, Container, Form } from "react-bootstrap";
 import Layout from "../../components/Layout";
 import Input from "../../components/UI/Input";
+import { login } from "../../redux/actions";
+import { useDispatch } from "react-redux";
 
 const Signin = (props) => {
+  const dispatch = useDispatch();
+  const userLogin = (e) => {
+    e.preventDefault();
+    const user = {
+      email: "jay123@email.com",
+      password: "123456",
+    };
+    dispatch(login(user));
+  };
+
   return (
     <Layout>
       <Container>
         <Row style={{ margin: "50px" }}>
           <Col md={{ span: 6, offset: 3 }}>
-            <Form>
+            <Form onSubmit={userLogin}>
               <Input
                 label="Email"
                 placeholder="Email"
