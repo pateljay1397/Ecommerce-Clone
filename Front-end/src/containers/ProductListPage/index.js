5abdf736866a906fd2227e5a87d9066c78871ee3
Jay Patel <pateljaypj1397@gmail.com>
2020-12-12 18:28:03 -0500

    Products by price range

--- Front-end/src/containers/ProductListPage/index.js
+++ Front-end/src/containers/ProductListPage/index.js
@@ -1,15 +1,61 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProductsBySlug } from "../../redux/actions";
 import Layout from "../../components/Layout";
-import { getProdcutsBySlug } from "../../redux/actions";
+import "./style.css";
+import { generatePublicUrl } from "../../urlConfig";
 
 const ProductListPage = (props) => {
+  const product = useSelector((state) => state.product);
+  const [priceRange, setPriceRange] = useState({
+    under$300: 300,
+    under$1000: 1000,
+    under$1500: 1500,
+    under$2000: 2000,
+    above$2000:2001
+  });
   const dispatch = useDispatch();
   useEffect(() => {
     const { match } = props;
-    dispatch(getProdcutsBySlug(match.params.slug));
+    dispatch(getProductsBySlug(match.params.slug));
   }, []);
-  return <Layout>ProductListPage</Layout>;
+
+  return (
+    <Layout>
+      {Object.keys(product.productsByPrice).map((key, index) => {
+        return (
+          <div className="card">
+            <div className="cardHeader">
+              <div>{props.match.params.slug} mobile under CAD ${priceRange[key]} </div>
+              <button>View all</button>
+            </div>
+            <div style={{ display: "flex" }}>
+              {product.productsByPrice[key].map((product) => (
+                <div className="productContainer">
+                  <div className="productImgContainer">
+                    <img
+                      src={generatePublicUrl(product.productPictures[0].img)}
+                      alt=""
+                    />
+                  </div>
+                  <div className="productInfo">
+                    <div style={{ margin: "5px 0", fontWeight: "bold" }}>
+                      {product.name}
+                    </div>
+                    <div>
+                      <span>4.3</span>&nbsp;
+                      <span>500</span>
+                    </div>
+                    <div className="productPrice">CAD ${product.price}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        );
+      })}{" "}
+    </Layout>
+  );
 };
 
 export default ProductListPage;
--- Front-end/src/redux/actions/product.action.js
+++ Front-end/src/redux/actions/product.action.js
@@ -1,19 +1,20 @@
 import axios from "../../helpers/axios";
 import { productConstants } from "./constants";
 
-export const getProdcutsBySlug = (slug) => {
+export const getProductsBySlug = (slug) => {
   return async (dispatch) => {
+    console.log("Action Called");
     const res = await axios.get(`/products/${slug}`);
-    if(res.status===200){
-        dispatch({
-            type: productConstants.GET_PRODUCTS_BY_SLUG_REQUEST,
-            payload: res.data
-        })
-    } else{
-        // dispatch({
-        //     type:
-        // })
+    if (res.status === 200) {
+      dispatch({
+        type: productConstants.GET_PRODUCTS_BY_SLUG_REQUEST,
+        payload: res.data,
+      });
+    } else {
+      // dispatch({
+      //     type:
+      // })
     }
-//    console.log(res);
+    console.log(res);
   };
 };
